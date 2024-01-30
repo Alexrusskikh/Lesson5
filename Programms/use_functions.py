24d2322642570ebf7d6ae1a4ca81a48bfeccd3af
Alex <alex_russ@bk.ru>
2024-01-30 16:07:38 +0500

    update file manager

--- Programms/use_functions.py
+++ Programms/use_functions.py
@@ -5,44 +5,57 @@ traffic_money = []
 
 
 # разделитель для  удобства восприятия
-def separator(symbol, count):
-    return f'{symbol}' * count
+def separator(simbol, count):
+    return simbol * count
 
 
 # функция пополнения  счета
-def put_cash(traffic_money):  # функция пополнения  счета
-    amount = int(input('Введите сумму для пополнения: '))
+def put_cash(amount_of_money):  # функция пополнения  счета
+    amount = input('Введите сумму для пополнения: ')
+    while not amount.isdigit():
+        amount = input('Пожалуйста, введите число....')
+
     traffic_money.append(
         ('Поступление средств ' + data, int(amount)))  # добавляет в traffic_money кортеж из 2 элементов
+    print(f'Внесена сумма {amount} УЕ')
+    input('\nНажмите Enter чтобы продолжить ')
 
 
 # функция покупки и ее суммы
-def buy(traffic_money):
+def buy(amount_of_money):
+    balans = sum([trans[1] for trans in traffic_money])
     product_name = input('Введите название продукта: ')
-    amount = input('Введите сумму покупки: ')
-    return traffic_money.append(
-        ('Покупка ' + data + ' ' + product_name, -int(amount)))  # добавляет в traffic_money покупки и ее суммы
+    amount = int(input('Введите сумму покупки: '))
+    if amount <= balans:
+        traffic_money.append(
+            ('Покупка ' + data + ' ' + product_name, -int(amount)))  # добавляет в traffic_money покупки и ее суммы
+    else:
+        print('Вы превышаете баланс счета, транзакция отменяется...')
+
+    input('\nНажмите Enter чтобы продолжить ')
 
 
 def history(traffic_money):  # функция истории транзакций
     print("Баланс счета: ", sum([trans[1] for trans in traffic_money]))
     print('История транзакций:')
     for name, amount in traffic_money:
-        print(f'{name}: {amount} УЕ')  # перебор кортежей и вывод их элементов через ":"
+        print(f'      {name}: {amount} УЕ')  # перебор кортежей и вывод их элементов через ":"
 
     input('\nНажмите Enter чтобы продолжить ')
 
 
-data = (datetime.date.today().strftime("%d/%m/%Y"))
-print(data)
-if __name__ == '__main__':
-    print(separator('*', 21))
+def show_date():
+    return (datetime.date.today().strftime("%d.%m.%Y"))
+
+
+# if __name__ == '__main__':
+#     print(separator('*', 21))
 
 
 def my_money():
     while True:
+        print(separator('*', 21))
         print("Баланс счета: ", sum([trans[1] for trans in traffic_money]))
-        print(traffic_money)
         print('1. пополнение счета')
         print('2. покупка')
         print('3. история покупок')
@@ -57,6 +70,7 @@ def my_money():
         elif choice == '3':
             history(traffic_money)
         elif choice == '4':
+            print('До новых встреч...')
             break
         else:
             print('Неверный пункт меню')
--- console file manager.py
+++ console file manager.py
@@ -1,20 +1,29 @@
+"""Консольный файловый менеджер
+
+"""
+import platform
 import os
+from collections import namedtuple
 import sys
 import shutil
 from Programms.use_functions import separator
+from Programms.use_functions import my_money
+from Programms.use_functions import show_date
 
+print(f'Текущая дата: {show_date()}')
 print(separator('*', 20))
 print("Меню: ")
 
 while True:
+    print('0. в какой я директории?')
     print('1. создать папку/файл')
     print('2. удалить (файл/папку)')
     print('3. копировать (файл/папку)')
     print('4. просмотр содержимого рабочей директории')
-    print('5. просмотреть только папки')
-    print('6. просмотреть только файлы')
+    print('5. просмотреть только файлы')
+    print('6. просмотреть только папки')
     print('7. просмотр информации об операционной системе')
-    print('8. создатель программы')
+    print('8. информация о файле')
     print('9. играть в викторину')
     print('10. мой банковский счет')
     print('11. смена рабочей директории')
@@ -23,12 +32,17 @@ while True:
 
     choice = input('Выберите пункт меню: ')
     print(separator('*', 20))
-    if choice == '1':
-        new_folder = input('Создать папку (введите путь): ')
+
+    if choice == '0':
+        print("Текущая деректория:", os.getcwd())
+
+    elif choice == '1':
+        new_folder = input('Создать папку в текущей директории (введите название): ')
         if not os.path.exists(new_folder):
             os.mkdir(new_folder)
         else:
             print('Такая папка существует!')
+
     elif choice == '2':
         del_folder = input('Удалить папку (введите путь): ')
         if os.path.exists(del_folder):
@@ -36,15 +50,19 @@ while True:
         else:
             print('Такой папки нет!')
     elif choice == '3':
+        print("Текущая деректория:", os.getcwd())
+        review_dir = os.listdir()
+        for el in review_dir:
+            print(el)
         new_round = "да"
-        while new_round == "да":
-            path_copy = input('Введите путь исходного файла: ')
-            if os.path.exists(path_copy):
-                name_copy = input('Введите путь нового файла: ')
-                if os.path.exists(name_copy):
+        while new_round == "да" or new_round == "Да":
+            exist_path_copy = input('Введите путь исходного файла: ')
+            if os.path.exists(exist_path_copy):
+                new_path_copy = input('Введите путь нового файла: ')
+                if os.path.exists(new_path_copy):
                     print('Такой файл ЕСТЬ!')
                     break
-                shutil.copy(path_copy, name_copy)
+                shutil.copy(exist_path_copy, new_path_copy)
                 print('Операция совершена!')
             else:
                 print('Такого файла НЕТ!')
@@ -52,31 +70,38 @@ while True:
             new_round = new_round.lower()
     elif choice == '4':
         print(os.listdir())
+
     elif choice == '5':
+        print("Текущая деректория:", os.getcwd())
         all_objects = os.listdir()
-        only_files = []
+        print('Текущая папка содержит файлы: ')
         for object in all_objects:
             if os.path.isfile(object):
-                only_files.append(object)
-        print(only_files)
+                print(object)
+
     elif choice == '6':
+        print("Текущая деректория:", os.getcwd())
         all_objects = os.listdir()
-        only_dir = []
+        print('Текущая папка содержит папки: ')
         for object in all_objects:
             if os.path.isdir(object):
-                only_dir.append(object)
-        print(only_dir)
+                print(object)
+
     elif choice == '7':
-        env = os.environ
-        for key, val in env.items():
-            print(key, '--> ', val)
+        platform_info = platform.uname()
+        platform_info_dict = platform_info._asdict()
+        num = 1
+        for k, v in platform_info_dict.items():
+            print(f' {num}. {k} = {v}')
+            num += 1
+
     elif choice == '8':
         size_file = input('Введите путь к нужному файлу: ')
         print('Размер файла: ', os.stat(size_file).st_size)
     elif choice == '9':
         from Programms import victory
     elif choice == '10':
-        from Programms import use_functions
+        my_money()
     elif choice == '11':
         print ('Текущая папка: ', os.getcwd())
         new_path = input('Перейти в: ')
